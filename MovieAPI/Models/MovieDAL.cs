506bd7042beba6ad3112f74f622642a60cc41a87
Alex Young <alexanderjyoung15@gmail.com>
2023-11-07 14:54:51 -0500

    Add project files.

---
+++ MovieAPI/Models/MovieDAL.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+
+namespace MovieAPI.Models
+{
+    public class MovieDAL
+    {
+        public static Movie GetMovie(string Title)
+        {
+            string url = $@"http://www.omdbapi.com/?t={Title}";
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest();
+            request.AddParameter("apikey", "1de70cac"); // You'll need to sign up for an OMDB API key
+
+            Movie output = client.Get<Movie>(request);
+
+            return output;
+        }
+    }
+
+}
